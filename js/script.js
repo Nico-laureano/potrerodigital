31ea7971823bedc842a74cacaad740d6982e9f7a
Nico-laureano <89483109+Nico-laureano@users.noreply.github.com>
2021-11-01 13:23:24 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+function activarOn() {
+  document.body.style.backgroundColor = "#252525";
+  document.getElementById("Alert-Modo-Oscuro").style.display = "none";
+  document.getElementById("Menu-nav").style.color = "#fff";
+  document.getElementById("Menu-nav__2").style.color = "#fff";
+  document.getElementById("Menu-nav__3").style.color = "#fff";
+  document.getElementById("Menu-nav__4").style.color = "#fff";
+  document.getElementById("Menu-nav__5").style.color = "#fff";
+  document.body.style.color = "#fff";
+  document.getElementById("Footer").style.backgroundColor = "#92ebbe38";
+  document.getElementById("card__1").style.border = "none";
+  document.getElementById("card__1").style.background = "#353535";
+  document.getElementById("card__2").style.border = "none";
+  document.getElementById("card__2").style.background = "#353535";
+  document.getElementById("card__3").style.border = "none";
+  document.getElementById("card__3").style.background = "#353535";
+  document.getElementById("card__4").style.border = "none";
+  document.getElementById("card__4").style.background = "#353535";
+  document.getElementsByName("h5").style.color = "#fff";
+
+}
+
+
+function DatosCreador() {
+  alert("Nicolas Sanchez // nsanchez@gmail.com // +54 11 57483839");
+}
